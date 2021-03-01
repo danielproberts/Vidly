c9ca2c420822f70f7f9ec9f463db473deb353503
Daniel Roberts <daniel.prescott.roberts@gmail.com>
2021-03-01 13:15:32 -0500

    start section 5 exercise

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -19,12 +19,80 @@ namespace Vidly.Controllers
         }
         public ViewResult Index()
         {
-            //var movies = GetMovies();
-
-            //return View(movies);
             var movies = _context.Movies.Include(m => m.Genre).ToList();
             return View(movies);
         }
+
+        public ActionResult New()
+        {
+            var genres = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel()
+            {
+                Genres = genres
+            };
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if(movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
+        }
+
+        /*[HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if(customer.Id == 0)
+            {
+                _context.Customers.Add(customer);
+            }
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+
+                customerInDb.Name = customer.Name;
+                customerInDb.BirthDate = customer.BirthDate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }*/
 
         public ActionResult Random()
         {
@@ -44,10 +112,12 @@ namespace Vidly.Controllers
             return View(viewModel);
         }
 
+        /*
         public ActionResult Edit(int id)
         {
             return Content("id=" + id);
         }
+        */
 
         public ActionResult Details(int id)
         {
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -10,14 +10,20 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        [Required]
         public Genre Genre { get; set; }
+
+        [Required]
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
-        [Required]
+
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+
         [Required]
+        [Display(Name = "Date Added")]
         public DateTime DateAdded { get; set; }
         [Required]
+        [Display(Name = "Number In Stock")]
         public int NumberInStock { get; set; }
     }
 }
